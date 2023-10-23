c9fbd373f83b6361f9b362166d819f6b199559e7
Shuren Xie <s2500272@bioinfmsc7.bio.ed.ac.uk>
2023-10-23 07:02:53 +0100

    added sequence assess module and part of the reads alignment counting module

--- pipeline.sh
+++ pipeline.sh
@@ -1,39 +1,102 @@
+#!/bin/bash
+
+#file paths
+fqs="/localdisk/data/BPSM/ICA1/fastq"
+genome="/localdisk/data/BPSM/ICA1/Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.gz"
+genes="/localdisk/data/BPSM/ICA1/TriTrypDB-46_TcongolenseIL3000_2019.bed"
+
 #doing the fastq jobs
-gzip -dc /localdisk/data/BPSM/ICA1/fastq/*.gz >> ${HOME}/BPSM/ICA1/fastq/
+gzip -dc $fqs/*.gz >> ${HOME}/BPSM/ICA1/fastq/
 cd ${HOME}/BPSM/ICA1/fastq
 fastq *.fq
 
-#doing the bowtie2 jobs
-cd ${HOME}/BPSM/ICA1
-mkdir bowtie2
-#unzip the fasta file
-gzip -dc /localdisk/data/BPSM/ICA1/Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.gz >> ${HOME}/BPSM/ICA1/bowtie2/tcongo_genome.fasta
-#constructing index for bowtie2
-bowtie2-build tcongo_genome.fasta reference_index
+#copy the sample details
+cp $fqs/Tco2.fqfiles ${HOME}/BPSM/ICA1/
+
+#check the quality of each gene
+for file in ${HOME}/BPSM/ICA1/htmls/*.html
+do
+	#check if there is any failure in base sequence content
+	if grep -q '\[FAIL\].*Per base sequence content' ${file}; then
+    echo "${file} There might be adapter contamination in the sequences."
+	fi
+done
+
 
 #using hisat2 for alignment of RNA seqences
 cd ${HOME}/BPSM/ICA1
 mkdir hisat2
-#unzip the fasta file
-gzip -dc /localdisk/data/BPSM/ICA1/Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.gz >> ${HOME}/BPSM/ICA1/hisat2/tcongo_genome.fasta
-#constructing index for hisat2
+##unzip the fasta file
+gzip -dc $genome >> ${HOME}/BPSM/ICA1/hisat2/tcongo_genome.fasta
+##constructing index for hisat2
+cd ${HOME}/BPSM/ICA1/hisat2
 hisat2-build tcongo_genome.fasta reference_index
-#executing hisat2 alignment with loop
-for file in ${HOME}/BPSM/ICA1/fastq
+##executing hisat2 alignment with loop
+for file in ${HOME}/BPSM/ICA1/fastq/*.fq
 do
 	base_name=$(basename "$file" .fq)
 	hisat2 -x reference_index -U ${file} -S ${HOME}/BPSM/ICA1/hisat2/${base_name}.sam
 done
 
-#converting to bam files
-for file in ${HOME}/BPSM/ICA1/hisat2
+#converting sam files to bam files
+for file in ${HOME}/BPSM/ICA1/hisat2/*.sam
 do
 	base_name=$(basename "$file" .sam)
-	samtools view -bS ${file} > ${HOME}/BPSM/ICA1/hisat2/${base_name}.bam
+	samtools view -b ${file} > ${HOME}/BPSM/ICA1/hisat2/${base_name}.bam
 done
 
-#counting the number of reads that aligned to the coding region
-cp /localdisk/data/BPSM/ICA1/TriTrypDB-46_TcongolenseIL3000_2019.bed ${HOME}/BPSM/ICA1/ttt.bed
+#sorting the bam files
+for file in ${HOME}/BPSM/ICA1/hisat2/*.bam
+do
+	base_name=$(basename "$file" .bam)
+	samtools sort ${file} -o sorted_${base_name}.bam
+done
+
+#counting the number of reads that aligned to the coding region of the genome
 cd ${HOME}/BPSM/ICA1
+mkdir counting
+cd ${HOME}/BPSM/ICA1/counting
+cp $genes ./genes.bed
+
+#generate counting results for each gene
+for file in ${HOME}/BPSM/ICA1/hisat2/sorted_*.bam
+do
+	base_name=$(basename "$file" .bam)
+	bedtools coverage -a genes.bed -b ${file} -counts > ${base_name}.coveredoutput
+done
+
+#sum up to get counts for the whole genome
+for file in ${HOME}/BPSM/ICA1/counting/*.coveredoutput
+do
+    base_name=$(basename "$file" .coveredoutput)
+    realname="${base_name#sorted_}"
+    awk '{counts += $NF} END {print counts}' "$file" | sed "s/^/${realname} /" >> counting.txt
+done
+
+#calculate mean counts for each gene
+#cut off verbose info first
+for file in ${HOME}/BPSM/ICA1/counting/*.coveredoutput
+do
+    base_name=$(basename "$file" .coveredoutput)
+    realname="${base_name#sorted_}"
+	awk -F'\t' '{print $4, $5, $6}' $file > $realname.count
+done
+
+#sum up counts by gene to get expression level
+awk '
+{
+    sum[$2] += $NF;
+    line[$2] = $0
+}
+END {
+    for (id in sum) {
+        sub(/[0-9]+$/, sum[id], line[id]);
+        print line[id]
+    }
+}
+' *.count > gene_expression_level.txt
+
+#
+
 
 
